96b8513078ef1814f9cec136555bc68e642cf1d0
sebastian slutzky <sebastianslutzky@yahoo.com>
2019-03-04 15:03:09 +0000

    [layout] do not show tab if there aren't elements in it

--- src/app/layout-column.component.ts
+++ src/app/layout-column.component.ts
@@ -11,6 +11,21 @@ export class LayoutColumnComponent  extends LayoutBaseComponent implements OnIni
   private FieldSets: any;
   private collections: any;
 
+  private isNotEmpty(tab: any): boolean {
+    const row = tab.row[0];
+    if (!row) { return false; }
+
+    const cols = row.cols[0];
+    if (!cols) { return false; }
+
+  if (!cols.col.fieldSet) {return false; }
+   const fieldSet = cols.col.fieldSet[0];
+
+   if (!fieldSet) { return false; }
+
+   return fieldSet.property || fieldSet.action;
+  }
+
   ngOnInit() {
      this.TabGroups = this.asArray(this.LayoutContext.tabGroup);
      this.FieldSets = this.asArray(this.LayoutContext.fieldSet);
